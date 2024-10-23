924600b71b9bef4475a42cc3a4a853f3c0ec575a
RenzoRomeo <renzoromeo44@gmail.com>
2024-10-23 12:21:26 -0300

    fix: location timeout

--- src/features/location/location.tsx
+++ src/features/location/location.tsx
@@ -1,9 +1,25 @@
-import { getLastKnownPositionAsync } from "expo-location";
+import {
+  Accuracy,
+  getCurrentPositionAsync,
+  getLastKnownPositionAsync,
+} from "expo-location";
+
+const LOCATION_TIMEOUT_MINUTES = 5;
 
 export async function getLocation() {
   try {
-    const location = await getLastKnownPositionAsync({});
-    return location;
+    const lastKnown = await getLastKnownPositionAsync();
+
+    if (
+      lastKnown &&
+      Date.now() - lastKnown.timestamp < LOCATION_TIMEOUT_MINUTES * 60 * 1000
+    ) {
+      return lastKnown;
+    }
+
+    return await getCurrentPositionAsync({
+      accuracy: Accuracy.BestForNavigation,
+    });
   } catch (err) {
     console.error(err);
     return undefined;
